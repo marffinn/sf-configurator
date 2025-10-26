b68a8a11a43923a896e5487e1e070f89006a1132
marffinn <marffinn@gmail.com>
2025-10-26 14:57:39 +0100

    j

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-  
-  webpack: {
-    configure: (config) => {
-      const preRule = config.module.rules.find(
-        (r) => r.enforce === 'pre' && r.use && r.use.some((u) => u.loader && u.loader.includes('source-map-loader'))
-      );
-      if (preRule) {
-        preRule.exclude = [
-          ...(preRule.exclude || []),
-          /@mediapipe[\\/]tasks-vision[\\/]vision_bundle\.mjs$/,
-        ];
-      }
-      return config;
-    },
-  },
-};
+module.exports = {};
